55edd29009dfdae616d545908959dd8377823de3
unknown <snanu15@freeuni.edu.ge>
2018-08-14 15:33:28 +0200

    initial

---
+++ Assets/CameraSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour {
+    private GameObject player;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/sprites/PlayerMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private int playerSpeed = 10;
+    private bool facingRight = false;
+    private int playerJumpPower = 1200;
+    private float moveX;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+	}
+
+    void Move() 
+    {
+        //controls
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        //directions
+        if (moveX < 0.0f && !facingRight)
+        {
+            FlipPlayer();
+        }
+        else if(moveX > 0.0f && facingRight)
+        {
+            FlipPlayer();
+        }
+        //physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
