56b41d2d2009892262e7734d3de7b3af5940cec5
unknown <snanu15@freeuni.edu.ge>
2018-08-15 10:32:43 +0200

    simple enemy chases you now

--- Assets/sprites/EnemyMove.cs
+++ Assets/sprites/EnemyMove.cs
@@ -6,14 +6,52 @@ using UnityEngine.SceneManagement;
 public class EnemyMove : MonoBehaviour {
     public int enemySpeed;
     public int xMoveDirection;
+    public Transform target;
+    const float MINDIST = 1.06f;
 	
 	// Update is called once per frame
-	void Update () {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMoveDirection, 0));
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMoveDirection, 0) * enemySpeed;
-        if(hit.distance < 0.7f)
+    void Start()
+    {
+        // if no target specified, assume the player
+        if (target == null)
         {
-            Debug.Log(hit.collider.tag.ToString());
+            if (GameObject.FindWithTag("Player") != null)
+            {
+                target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+            }
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMoveDirection, 0) * enemySpeed;
+        if (target == null)
+            return;
+
+        // face the target
+        //transform.LookAt(target);
+
+        //get the distance between the chaser and the target
+        float distance = Vector2.Distance(transform.position, target.position);
+
+        //so long as the chaser is farther away than the minimum distance, move towards it at rate speed.
+        if (distance < MINDIST)
+        {
+            SceneManager.LoadScene("Scene1");
+        }
+
+        if(target.position.x > transform.position.x && xMoveDirection < 0 
+            || target.position.x < transform.position.x && xMoveDirection > 0) 
+        {
+            xMoveDirection *= -1;
+        }
+    }
+            
+
+    // Set the target of the chaser
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
     }
 }
